c399f1c37b0a89e2111e5b5509bb579478189292
dannyadm <137915063+dannyadm@users.noreply.github.com>
2025-02-14 15:00:06 -0500

    cambios para probar melos

---
+++ assets/js/base_danny.js
@@ -0,0 +1,45 @@
+document.addEventListener('DOMContentLoaded', function () {
+
+    const video = document.getElementById('video');
+    const canvas = document.getElementById('canvas');
+    const photo = document.getElementById('photo');
+    const captureButton = document.getElementById('capture');
+
+    function startCamera() {
+        navigator.mediaDevices.getUserMedia({ video: true })
+            .then(stream => { video.srcObject = stream; })
+            .catch(err => console.error("Error al acceder a la cámara: ", err));
+    }
+
+    function capturePhoto() {
+        contador =+ 1
+        const context = canvas.getContext('2d');
+        canvas.width = 435;
+        canvas.height = 290;
+        //context.drawImage(video, 0, 0, canvas.width, canvas.height);
+        context.drawImage(video, 100,85, 435, 290, 0, 0, 435, 290);
+
+        // Convertir la imagen a base64 y mostrarla
+        photo.src = canvas.toDataURL('image/png');
+    }
+
+    //setInterval(capturePhoto, 5000);
+    
+
+    captureButton.addEventListener('click', function() {
+        let contador = 0
+
+        let interval = setInterval(() => {
+            if (contador < 5) {
+                capturePhoto()
+                contador += 1
+            }else {
+                clearInterval(interval)
+                console.log('Finalizo capturas');
+            }
+        }, 3000)
+        
+    });
+
+    startCamera()
+})
