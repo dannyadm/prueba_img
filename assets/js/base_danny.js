4fdd4e5772d6a778448a87c338381e8755205a3e
dannyadm <137915063+dannyadm@users.noreply.github.com>
2025-02-17 17:40:23 -0500

    caaaa

--- assets/js/base_danny.js
+++ assets/js/base_danny.js
@@ -139,9 +139,11 @@ document.addEventListener('DOMContentLoaded', function () {
         context.save(); // Guardar el estado actual del contexto
     
         if (isMobile && portrait) {
-            // Rotar 180° la imagen para corregir la orientación en móviles
-            context.translate(canvas.width, canvas.height);
-            context.rotate(Math.PI);
+            // Rotar 90° la imagen para corregir la orientación en móviles
+            context.translate(canvas.width, 0);  // Mover el origen del contexto al borde derecho
+            context.rotate(Math.PI / 2);  // Rotar 90° en sentido horario
+            canvas.width = video.videoHeight;  // Ajustar el ancho del canvas al nuevo tamaño
+            canvas.height = video.videoWidth;  // Ajustar el alto del canvas al nuevo tamaño
         }
     
         context.drawImage(video, 0, 0, canvas.width, canvas.height);
