92de06c1b1c1810bc0ae5384345d008dda907f71
dannyadm <137915063+dannyadm@users.noreply.github.com>
2025-02-20 15:50:38 -0500

    Cambios para recortessss

--- assets/js/camera.js
+++ assets/js/camera.js
@@ -114,28 +114,33 @@ document.addEventListener('DOMContentLoaded', function () {
         const cornerPoints = scanner.getCornerPoints(contour);
         console.log('Coordenadas obtenidasss:', cornerPoints);
 
+        const scaleFactor = window.devicePixelRatio || 1;
         let newSisze = getSizeNewImage(cornerPoints);
         console.log('Nuevas dimensioness', newSisze);
 
         const extractedCanvas = document.createElement('canvas');
-        extractedCanvas.width = newSisze.newWith;
-        extractedCanvas.height = newSisze.newHeight;
+        extractedCanvas.width = newSisze.newWith * scaleFactor;
+        extractedCanvas.height = newSisze.newHeight * scaleFactor;
         const extractedCtx = extractedCanvas.getContext('2d');
 
-        let isMobile = /Android|iPhone|iPad|iPod/i.test(navigator.userAgent);
-        if (isMobile) {
-            extractedCtx.drawImage(
-                imgElement,
-                cornerPoints.bottomLeftCorner.x, cornerPoints.bottomLeftCorner.y, newSisze.newWith, newSisze.newHeight,
-                0, 0, newSisze.newWith, newSisze.newHeight
-            );    
-        }else {
-            extractedCtx.drawImage(
-                imgElement,
-                cornerPoints.topLeftCorner.x, cornerPoints.topLeftCorner.y, newSisze.newWith, newSisze.newHeight,
-                0, 0, newSisze.newWith, newSisze.newHeight
-            );
-        }        
+        
+
+        const adjustedX = cornerPoints.topLeftCorner.x * scaleFactor;
+        const adjustedY = cornerPoints.topLeftCorner.y * scaleFactor;
+        const adjustedWidth = newSisze.newWith * scaleFactor;
+        const adjustedHeight = newSisze.newHeight * scaleFactor;
+
+        /*extractedCtx.drawImage(
+            imgElement,
+            cornerPoints.topLeftCorner.x, cornerPoints.topLeftCorner.y, newSisze.newWith, newSisze.newHeight,
+            0, 0, newSisze.newWith, newSisze.newHeight
+        );*/
+        extractedCtx.drawImage(
+            imgElement,  // Imagen original
+            adjustedX, adjustedY, adjustedWidth, adjustedHeight,
+            0, 0, extractedCanvas.width, extractedCanvas.height
+        );
+
 
         divRecort.src = extractedCanvas.toDataURL('image/png');
     }
