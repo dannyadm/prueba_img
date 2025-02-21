0f86af0f544c1c7e5940ff93428853bb6d179259
dannyadm <137915063+dannyadm@users.noreply.github.com>
2025-02-20 20:16:24 -0500

    Cambios para imagen inicial

--- assets/js/camera.js
+++ assets/js/camera.js
@@ -9,7 +9,7 @@ document.addEventListener('DOMContentLoaded', function () {
     const divResult = document.getElementById('result');
     const divProcces = document.getElementById('imgProccess');
     const divRecort = document.getElementById('imgResult');
-    const imgGrayScale = document.getElementById('imgGrayScale');
+    const canvasCapture = document.getElementById('canvasCapture');
     const imgRotated = document.getElementById('imgRotated');
     var takePhotoButtonElement = document.getElementById('takePhotoButtonId');
     var stopCameraButtonElement = document.getElementById('stopCameraButtonId');
@@ -44,7 +44,7 @@ document.addEventListener('DOMContentLoaded', function () {
         divVideo.style.display = 'none'
     }
 
-    function takePhoto() {
+    /*function takePhoto() {
         var config = {
             sizeFactor: 1,
             imageType: IMAGE_TYPES.PNG,
@@ -85,9 +85,71 @@ document.addEventListener('DOMContentLoaded', function () {
                 imgRotated.src = url;
             }, 'image/png');
 
-            //const base64Image = canvas.toDataURL('image/png');
-            //imgRotated.src = base64Image;
         }, 20);
+
+        stopCamera()
+    }*/
+
+    function takePhoto() {
+
+        /*canvasCapture.width = videoElement.videoWidth;
+        canvasCapture.height = videoElement.videoHeight;
+        const contexto = canvasCapture.getContext('2d');
+        contexto.imageSmoothingEnabled = true;
+        contexto.imageSmoothingQuality = "high";
+        contexto.drawImage(videoElement, 0, 0, canvasCapture.width, canvasCapture.height);
+        canvasCapture.toBlob(function (blob) {
+            var urlOrg = URL.createObjectURL(blob);
+            imgElement.src = urlOrg;
+        }, 'image/png');*/
+
+        //imgElement.src = dataUri;
+
+        var config = {
+            sizeFactor: 1,
+            imageType: IMAGE_TYPES.PNG,
+            imageCompression: 1
+        };
+
+        var dataUri = cameraPhoto.getDataUri(config);
+        console.log('Tipo de image de variable', typeof dataUri);
+        imgElement.src = dataUri;
+        setTimeout(() => {
+            let imgWidth = imgElement.width;
+            let imgHeight = imgElement.height;
+            console.log('Que hayyyy aquiiiiiiiii', imgWidth);
+            console.log('Que hayyyy aquiiiiiiiii', imgHeight);
+            const canvas = document.createElement('canvas');
+            const ctx = canvas.getContext('2d');
+            /*let isMobile = /Android|iPhone|iPad|iPod/i.test(navigator.userAgent);
+            if (isMobile) {
+                console.log('Si es un mobileeeee');
+                canvas.width = imgElement.height;
+                canvas.height = imgElement.width;
+                ctx.imageSmoothingEnabled = true;
+                ctx.imageSmoothingQuality = "high";
+                ctx.save();
+                ctx.translate(0, canvas.height);
+                ctx.rotate(Math.PI * 1.5);
+                ctx.drawImage(imgElement, 0, 0, imgWidth, imgHeight);
+            } else {
+                console.log('NO es un mobileeeee');
+                canvas.width = imgElement.width;
+                canvas.height = imgElement.height;
+                ctx.imageSmoothingEnabled = true;
+                ctx.imageSmoothingQuality = "high";
+                ctx.drawImage(imgElement, 0, 0);
+            }*/
+            ctx.drawImage(imgElement, 0, 0);
+            let mat = cv.imread(canvas);
+            cv.cvtColor(mat, mat, cv.COLOR_RGBA2GRAY);
+            cv.imshow(canvas, mat);
+            canvas.toBlob(function (blob) {
+                var url = URL.createObjectURL(blob);
+                imgRotated.src = url;
+            }, 'image/png');
+
+        }, 500);
 
         stopCamera()
     }
@@ -145,41 +207,41 @@ document.addEventListener('DOMContentLoaded', function () {
 
     function cutImage() {
         divResult.style.display = 'block';
-    
+
         const highlightedCanvas = scanner.highlightPaper(imgElement);
         divProcces.src = highlightedCanvas.toDataURL('image/png');
-    
+
         const contour = scanner.findPaperContour(cv.imread(imgElement));
         const cornerPoints = scanner.getCornerPoints(contour);
         console.log('Coordenadas obtenidasss:', cornerPoints);
-    
+
         const imgWidth = imgElement.naturalWidth;
         const imgHeight = imgElement.naturalHeight;
         const displayWidth = imgElement.width;
         const displayHeight = imgElement.height;
-    
+
         const scaleX = imgWidth / displayWidth;
         const scaleY = imgHeight / displayHeight;
-    
+
         let newSisze = getSizeNewImage(cornerPoints);
         console.log('Nuevas dimensioness', newSisze);
-    
+
         const extractedCanvas = document.createElement('canvas');
         extractedCanvas.width = newSisze.newWith;
         extractedCanvas.height = newSisze.newHeight;
         const extractedCtx = extractedCanvas.getContext('2d');
-    
+
         const adjustedX = cornerPoints.topLeftCorner.x * scaleX;
         const adjustedY = cornerPoints.topLeftCorner.y * scaleY;
         const adjustedWidth = (cornerPoints.topRightCorner.x - cornerPoints.topLeftCorner.x) * scaleX;
         const adjustedHeight = (cornerPoints.bottomLeftCorner.y - cornerPoints.topLeftCorner.y) * scaleY;
-    
+
         extractedCtx.drawImage(
             imgElement,  // Imagen original
             adjustedX, adjustedY, adjustedWidth, adjustedHeight,
             0, 0, extractedCanvas.width, extractedCanvas.height
         );
-    
+
         divRecort.src = extractedCanvas.toDataURL('image/png');
     }
 
